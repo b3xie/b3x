aaacd15d7c1e7cfc13565b195263f67fb49561be
Gabz@Arch <gabrasan@gmail.com>
2023-05-22 22:03:32 -0300

    chronological post order

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -29,18 +29,21 @@ function PostList() {
   return (
     <>
       <section className="flex flex-col space-y-4">
-        {posts.map((post) => {
-          return (
-            <div className="flex flex-col bg-bexdark w-auto min-w-[200px] p-3">
-              <div className="">
-                <h2 className="text-bexpurplemedium">{post.name}:</h2>
+        {posts
+          .slice(0)
+          .reverse()
+          .map((post) => {
+            return (
+              <div className="flex flex-col bg-bexdark w-auto min-w-[200px] p-3">
+                <div className="">
+                  <h2 className="text-bexpurplemedium">{post.name}:</h2>
+                </div>
+                <div className="">
+                  <h2 className="text-bexpurplelight">{post.post}</h2>
+                </div>
               </div>
-              <div className="">
-                <h2 className="text-bexpurplelight">{post.post}</h2>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
       </section>
     </>
   );
